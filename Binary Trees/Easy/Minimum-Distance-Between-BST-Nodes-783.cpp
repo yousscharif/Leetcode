b5b4f3dfdc66ba78d1cf6b19336f4eadb5ac41d5
yousscharif <youssecharif15@gmail.com>
2022-09-17 15:46:52 +0300

    🔥 Accepted

--- Binary Trees/Easy/Minimum-Distance-Between-BST-Nodes-783.cpp
+++ Binary Trees/Easy/Minimum-Distance-Between-BST-Nodes-783.cpp
@@ -18,3 +18,25 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+class Solution {
+public:
+    TreeNode* prev;
+    int minDiff = 1e9;
+
+    int minDiffInBST(TreeNode* root) {
+        prev = new TreeNode(1e9);
+        inOrder(root);
+        return minDiff;    
+    }
+    
+    void inOrder(TreeNode* root) {
+        if (root == nullptr) {
+            return;
+        }
+        inOrder(root->left);
+        minDiff = min(minDiff, abs(root->val - prev->val));
+        prev = root;
+        inOrder(root->right);
+    }
+};
