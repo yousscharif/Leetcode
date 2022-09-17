b8241a53a94df8d7ea97c8f864d8bc5e5e0fdde3
yousscharif <youssecharif15@gmail.com>
2022-09-17 10:49:48 +0300

    🔥 Accepted

---
+++ Binary Trees/Easy/Minimum-Distance-Between-BST-Nodes-783.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <list>
+
+using namespace std;
+
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
